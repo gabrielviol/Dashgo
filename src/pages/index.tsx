67d1d36694072f74d6af08ee4b914d4b65b725a5
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-11-23 15:15:40 -0300

    add eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,24 @@
-export default function Home()  {
+import { Button, Flex, Input } from '@chakra-ui/react'
+
+export default function Home() {
   return (
-    <h1>DashGo</h1>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Input name="email" type="email" />
+        <Input name="password" type="password" />
+
+        <Button type="submit" mt="6" colorScheme="pink">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
