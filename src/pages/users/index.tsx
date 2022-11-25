b4d436449197aa04f68e65eece61b2098cc7d885
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-11-25 14:31:16 -0300

    create component: fineshed pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -23,9 +23,8 @@ export default function UserList() {
     <Box>
       <Header />
 
-      <Flex>
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
-
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
