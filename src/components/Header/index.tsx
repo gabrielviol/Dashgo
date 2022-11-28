96aa8e242f90274857af2fb6dfd318394da5463e
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-11-28 18:00:39 -0300

    responsiveness: Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -23,7 +23,7 @@ export function Header() {
     >
       <Logo />
 
-      <SearchBox />
+      {isWideVersion && <SearchBox />}
 
       <Flex align="center" ml="auto">
         <NotificationsNav />
