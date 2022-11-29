3fcedd709c309550ec92ae6e8829296fd92f0852
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-11-29 20:00:45 -0300

    forms: create form with react hook form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import { Button, Flex, Stack } from '@chakra-ui/react'
+import { useForm } from 'react-hook-form'
 import { Input } from '../components/Form/input'
 
 export default function Home() {
+  const { register, handleSubmit } = useForm()
+
+  function handleSignIn(values) {
+    console.log(values)
+  }
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -12,11 +19,12 @@ export default function Home() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" label="E-mail" />
+          <Input type="email" label="E-mail" {...register('email')} />
 
-          <Input name="password" type="password" label="Senha" />
+          <Input type="password" label="Senha" {...register('password')} />
         </Stack>
 
         <Button type="submit" mt="6" colorScheme="pink" size="lg">
